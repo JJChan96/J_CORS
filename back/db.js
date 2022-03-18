7fe20a21c430bca35d930c7fbf2b2a6d8ef94131
juchan <jucham96@gmail.com>
2022-03-18 12:48:40 +0900

    에라

--- back/db.js
+++ back/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2')
 // npm install mysql2
 
 const host = process.env.DB_HOST || 'localhost'
-const user = process.env.DB_USER || 'ingoo2'
-const password = process.env.DB_PASSWORD || 'ingoo2'
+const user = process.env.DB_USER || 'juchan'
+const password = process.env.DB_PASSWORD || 'juchan'
 const database = process.env.DB_DATABASE || 'home'
 
 
